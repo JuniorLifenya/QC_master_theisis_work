6360e00090d9f11668c42303fc62f62ef5dd99d5
JuniorLifenya <Danyelfrilan345@outlook.com>
2025-06-17 19:44:16 +0200

    Starten på noe vakkert ... Quantum computing

---
+++ qiskitcoding.py
@@ -0,0 +1,11 @@
+from qiskit import QuantumCircuit, Aer, execute
+
+qc = QuantumCircuit(1, 1)
+qc.h(0)
+qc.measure(0, 0)
+
+backend = Aer.get_backend('qasm_simulator')
+job = execute(qc, backend, shots=1024)
+result = job.result().get_counts()
+
+print(result)
