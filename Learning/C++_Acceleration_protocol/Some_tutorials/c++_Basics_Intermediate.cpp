63a23a46b5c36f35b98b1a86929bdaf36723a1b1
JuniorLifenya <Danyelfrilan345@outlook.com>
2025-08-08 09:31:15 +0200

    Namespcae continued tutorial

--- Learning/C++_Acceleration_protocol/Some_tutorials/c++_Basics_Intermediate.cpp
+++ Learning/C++_Acceleration_protocol/Some_tutorials/c++_Basics_Intermediate.cpp
@@ -3,6 +3,21 @@
 // main is the entrypoint of our program.
 #include <cmath>
 
+namespace first
+{
+    int x = 0; // This is a variable in the first namespace
+
+}
+
+namespace second
+{
+    int x = 1; // This is a variable in the second namespace
+}
+
+// To use the variables from the namespaces, we can use the scope resolution operator ::
+// Inside int main() we can access them like this:
+// first::x and second::x
+
 int main() // Int is the type of value returned by main.
 {
     // ======================= Intro with printing ===========================
@@ -43,7 +58,31 @@ int main() // Int is the type of value returned by main.
     cout << "You entered: " << value << endl;
     // prints the value, if not integer , it will round to integer or print 0
 
-        return 0;
+    //=======================================================================
+    //===================== Namespace =======================================
+    // Namespace first appeared in C++ to avoid name conflicts.
+    // std is the standard namespace, it contains all the standard library functions and classes.
+    // To avoid having to redfine new variables like this
+
+    int x = 0;
+    int x = 1; // This will not compile, because x is already defined in the same scope
+    // To avoid this, we can use different namespaces or use the std namespace.
+
+    cout << first ::x << std ::endl;  // Accessing variable from first namespace
+    cout << second ::x << std ::endl; // Accessing variable from second namespace
+
+    //===================== Different namespace =============================
+
+    using first::x;      // Can also declare namespace usage inside main
+    using second::x;     // Can also declare namespace usage inside main
+    using std::cin;      // Using cin from the std namespace
+    using std::cout;     // Using cout from the std namespace
+    using namespace std; // Using the entire std namespace ( all of the above )
+
+    //=======================================================================
+    //===================== Different namespace =============================
+
+    return 0;
 }
 // std is short for standard,
 // it's a namespace that contains many useful functions and classes.
