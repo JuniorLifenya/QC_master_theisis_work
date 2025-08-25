70b3c21b058df3758faa31d2ae5283113a80cd22
JuniorLifenya <Danyelfrilan345@outlook.com>
2025-08-25 15:26:20 +0200

    small changes and new stuff added

--- Learning/C++_Acceleration_protocol/Mini_projects_exercise/Decoherence_Model/nv_decoherence.cpp
+++ Learning/C++_Acceleration_protocol/Mini_projects_exercise/Decoherence_Model/nv_decoherence.cpp
@@ -1,8 +1,20 @@
-// src/physics/nv_decoherence.cpp
-#include <Eigen/Dense>
-#include "gw_strain.hpp"
 
-Eigen::VectorXd calculate_decoherence(
+#include <iostream>
+#include <complex>
+#include "eigen-3.4.0/Eigen/Dense" // Updated include path
+#include "eigen-3.4.0/Eigen/Eigenvalues"
+#include <iomanip>
+#include <iostream> // For input and output operations
+#include <vector>
+#include <complex> // For complex number support
+#include <iterator>
+#include <Eigen/Dense> //Used for heacy matrix operations
+#include "gw_strain.hpp"
+namespace fs = std::filesystem;
+using namespace std;
+using namespace Eigen;
+using real = double;
+VectorXd calculate_decoherence(
     const Eigen::MatrixXcd &hamiltonian,
     double gw_amplitude,
     double frequency)
@@ -11,4 +23,11 @@ Eigen::VectorXd calculate_decoherence(
     Eigen::SelfAdjointEigenSolver solver(hamiltonian);
     // ... GW strain integration ...
     return decoherence_rates;
+}
+
+VectorXd S(const Mat)
+
+    MatrixXd FULL_NV_center_Hamiltonian // The full Hamiltonian is H_NV = DS^2_z + gamma\vector{S} + H_strain + H_GW
+{
+    const(D) * real(S) * real(S) + const(gamma) * S
 }
---
+++ Learning/C++_Acceleration_protocol/Mini_projects_exercise/Python_Bridge/python_Bridge.py
@@ -0,0 +1,5 @@
+
+
+print(" Olø \n")
+print(" Olø \n")
+print(" Olø")
