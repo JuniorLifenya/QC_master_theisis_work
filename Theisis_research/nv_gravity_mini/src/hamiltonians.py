773fe55c184d00ae5a63af99bc9480aa07276c4e
JuniorLifenya <Danyelfrilan345@outlook.com>
2025-09-06 20:17:42 +0200

    Hamiltonian started for nv center start

---
+++ Theisis_research/nv_gravity_mini/src/hamiltonians.py
@@ -0,0 +1,61 @@
+import numpy as np
+import qutip as qt
+
+# Define NV-center spin-1 operators
+Sx = qt.jmat(1, 'x')
+Sy = qt.jmat(1, 'y')
+Sz = qt.jmat(1, 'z')
+Sx2 = Sx * Sx
+Sy2 = Sy * Sy
+Sz2 = Sz * Sz
+
+# Zero-field splitting constant (in Hz)
+D = 2.87e9
+
+def get_base_hamiltonian(Bz=0.0):
+    """
+    Returns the static NV Hamiltonian H_0.
+    Args:
+        Bz (float): Magnetic field in Tesla (for Zeeman splitting)
+    """
+    H_0 = D * Sz2  # Zero-field term
+    if Bz != 0.0:
+        gamma_e = 28e9  # Electron gyromagnetic ratio (Hz/T)
+        H_0 += gamma_e * Bz * Sz  # Zeeman term
+    return H_0
+
+def get_interaction_hamiltonian(strain_plus, strain_cross):
+    """
+    Returns the interaction Hamiltonian H_int from gravitational strain.
+    THIS IS THE KEY PHYSICS YOU NEED TO DERIVE.
+    A first guess based on your supervisor's quadrupole comment:
+    H_int ~ h_plus(t) * (Sx^2 - Sy^2) + h_cross(t) * (Sx*Sy + Sy*Sx)
+
+    Args:
+        strain_plus (float): h_plus strain component
+        strain_cross (float): h_cross strain component
+    """
+    # This coupling constant 'kappa' is what you need to calculate/estimate from your theory!
+    kappa = 1e10  # Placeholder! This is a crucial parameter from your derivation.
+
+    H_int = kappa * (
+        strain_plus * (Sx2 - Sy2) +
+        strain_cross * (Sx * Sy + Sy * Sx)
+    )
+    return H_int
+
+def get_total_hamiltonian(t, args):
+    """
+    Function of time 't' for QuTiP to use in time-dependent problems.
+    'args' is a dictionary expected to contain:
+        'Bz': magnetic field
+        'h_plus_func': function h_plus(t)
+        'h_cross_func': function h_cross(t)
+    """
+    h_p = args['h_plus_func'](t)
+    h_c = args['h_cross_func'](t)
+    
+    H_0 = get_base_hamiltonian(args['Bz'])
+    H_int = get_interaction_hamiltonian(h_p, h_c)
+    
+    return H_0 + H_int
